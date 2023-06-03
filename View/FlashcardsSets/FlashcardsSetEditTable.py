47a45947f0ab5a57ef4fa77a1fb48c1fb2dfdc32
Marcin <marcin.tkocz@op.pl>
2023-06-04 00:51:09 +0200

    enabled enters and tabs in flashcardssetEditTable

--- View/FlashcardsSets/FlashcardsSetEditTable.py
+++ View/FlashcardsSets/FlashcardsSetEditTable.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QTableWidget, QLineEdit, QTableWidgetItem, QHeaderView, QAbstractItemView
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QTimer
 from View.ViewUtilities import set_widget_font_size
 
 class FlashcardSetEditTable(QTableWidget):   
@@ -11,13 +11,21 @@ class FlashcardSetEditTable(QTableWidget):
         self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.horizontalHeader().setSectionsClickable(False)
         self.horizontalHeader().setSectionsMovable(False)
-
+        self.cellClicked.connect(self.edit_cell)
         set_widget_font_size(self, 15)
 
-    def add_row_with_edit_finish(self):
-        row_count = self.rowCount()
-        self.insertRow(row_count)
-        #self.setCurrentCell(row_count, 0)
+    def edit_cell(self, row, col):
+        item = self.item(row, col)
+        if item is None:
+            item = QTableWidgetItem()
+            self.setItem(row, col, item)
+        self.setCurrentCell(row, 0)
+        item.setFlags(item.flags() | Qt.ItemIsEditable)
+        self.editItem(item)
+
+    def add_row_with_edit(self, row_idx):
+        self.insertRow(row_idx)
+        QTimer.singleShot(0, lambda: self.edit_cell(row_idx, 0))
 
     def load_set_for_edit(self, flashcards_set):
         self.clearContents()
@@ -28,17 +36,10 @@ class FlashcardSetEditTable(QTableWidget):
             self.setItem(row, 0, original)
             self.setItem(row, 1, translation)
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            index = self.indexAt(event.pos())
-            if index.isValid():
-                self.edit(index)
-
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
             current_index = self.currentIndex()
             current_row = current_index.row()
             current_column = current_index.column()
-
             if current_column == self.columnCount() - 1:
-                self.add_row_with_edit_finish()
+                self.add_row_with_edit(current_row+1)
